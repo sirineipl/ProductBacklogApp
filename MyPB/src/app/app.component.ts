81a45354ea06a341e710a8dc3dfabde9ae718be8
sirine <cyrinezaouali@gmail.com>
2022-05-12 21:47:52 +0200

    first commit

---
+++ MyPB/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogComponent } from './dialog/dialog.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'MyPB';
+
+  constructor(private dialog : MatDialog) {
+    
+  }
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+      width: '30%'
+      
+    });
+  }
+}
